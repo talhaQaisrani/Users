8afeecdadcfc0707f731910ee6528d0f5028c211
talhaQaisrani <talhajunaid65@gmail.com>
2017-07-26 10:03:46 +0500

    Kaminri Practice

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
+  get 'posts/index'
+
   devise_for :users
   resources :users
 
-  root to: "users#index"
+  #root to: "users#index"
+  root to: "posts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
