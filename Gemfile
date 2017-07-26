54905c9733e85e10209dfc5a23f733943cd7f1cb
talhaQaisrani <talhajunaid65@gmail.com>
2017-07-26 11:26:19 +0500

    Practicing Cocoon

--- Gemfile
+++ Gemfile
@@ -56,3 +56,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'devise'
 gem 'carrierwave', '~> 1.0'
 gem 'kaminari'
+gem 'cocoon'
+gem 'jquery-rails'
---
+++ db/migrate/20170726055530_create_tasks.rb
@@ -0,0 +1,10 @@
+class CreateTasks < ActiveRecord::Migration[5.1]
+  def change
+    create_table :tasks do |t|
+      t.string :decription
+      t.belongs_to :project, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
